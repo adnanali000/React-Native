6e01b01cffc942406562a6539f3d3828361ffc3a
adnanali000 <adnan.museel123@gmail.com>
2021-08-30 17:05:04 +0500

    list and scrollview work done

--- App.js
+++ App.js
@@ -1,28 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View,ScrollView } from 'react-native';
 
 export default function App() {
-  const [name,setName] = useState('adnan');
-  const [age,setAge] = useState('21');
+  const [people,setPeople] = useState([
+    {name:'adnan',key:'1'},
+    {name:'faham',key:'2'},
+    {name:'wasim',key:'3'},
+    {name:'suffyan',key:'4'},
+    {name:'najam',key:'5'},
+    {name:'tayyab',key:'6'},
+    {name:'fazeel',key:'7'},
+    {name:'anas',key:'8'},
+
+  ]);
+  
 
   return (
     <View style={styles.container}>
-      <Text>Enter Name:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder='e.g adnan'
-        multiline
-        onChangeText={(value)=>setName(value)}
-      />
-      <Text>Enter Age:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="e.g 25"
-        keyboardType="numeric"
-        onChangeText={(value)=>setAge(value)}
-      />
-        <Text>Name: {name}, age: {age}</Text>
+
+    <ScrollView>  
+     {
+       people.map((item)=>{
+         return(
+            <View key={item.key}>
+                <Text  style={styles.li}>{item.name}</Text>
+            </View>
+         )
+       })
+     }
+     </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -32,14 +39,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:40,
+    paddingHorizontal:20
+
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  input:{
-    borderWidth:1,
-    padding:10,
-    width:200,
-    margin:10
+  li:{
+    color:'white',
+    fontSize:24,
+    textAlign:"center",
+    marginTop:20,
+    padding:30,
+    backgroundColor:"red",
 
   }
  
